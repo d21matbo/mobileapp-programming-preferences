96ea2ee58a3bc528cb0a07313f3b3c8fabedcdae
Mattias Borgenstierna <d21matbo@student.his.se>
2022-05-03 11:56:44 +0200

    Implement SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        myPreferenceRef = getSharedPreferences("saved", MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        editText = findViewById(R.id.edit_text);
+
+        Button button = findViewById(R.id.button_second);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myPreferenceEditor.putString("savedPreference", editText.getText().toString());
+                myPreferenceEditor.apply();
+            }
+        });
     }
 }
